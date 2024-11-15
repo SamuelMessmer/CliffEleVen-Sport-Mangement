221b3652ed62c0e7a84177b80de962c04e2ac950
Samuel Messmer <samuel.messmer@web.de>
2024-11-15 16:59:39 +0100

    style: remake transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,8 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
+
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
