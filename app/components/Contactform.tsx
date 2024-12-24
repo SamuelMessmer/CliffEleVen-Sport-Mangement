e1c40be0254f4eff51c326ff6211bbfaefd5050e
Samuel Messmer <samuel.messmer@web.de>
2024-12-24 10:46:27 +0100

    chore: fix alignment

--- app/components/Contactform.tsx
+++ app/components/Contactform.tsx
@@ -131,6 +131,9 @@ const Contactform = () => {
                   className="text-white border-[#3550FF] border-2 rounded-xl px-4 py-1 font-extrabold"
                 >
                   Senden(unverbindlich)
+                  <span className="text-white font-xs" >Die von Ihnen im Formular angegebenen Daten werden ausschließlich dazu verwendet,
+                    um mit Ihnen in Kontakt zu treten. Eine Weitergabe an Dritte oder Nutzung für andere
+                    Zwecke erfolgt nicht.</span>
                 </button>
               </div>
               <div className="self-center font-bold text-lg">
