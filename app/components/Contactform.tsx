de8694911e18ff32910d6d06065bcc06765c1cb1
Samuel Messmer <samuel.messmer@web.de>
2024-12-24 14:07:18 +0100

    fix: bugs on auto deploy

--- app/components/Contactform.tsx
+++ app/components/Contactform.tsx
@@ -153,7 +153,6 @@ const Contactform = () => {
                     >
                       erneut versuchen
                     </Link>
-                    
                   </div>
                 )}
               </div>
